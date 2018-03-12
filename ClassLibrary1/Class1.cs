0e2bd194fcad45da49e5113c0c1c4f5e94bbaf4c
Vladimir <riuson@gmail.com>
2018-03-12 23:06:21 +0500

    Simulate application

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
+        public void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
